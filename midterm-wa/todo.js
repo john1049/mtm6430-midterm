0a8130927e82acea73fa5fe8c39612c991145666
LAPTOP\Ryan <john1049@algonquinlive.com>
2017-03-09 16:06:35 -0500

    Add localstorage function works, with Error: [ngModel:datefmt]

--- midterm-wa/todo.js
+++ midterm-wa/todo.js
@@ -1,11 +1,18 @@
 (function(){
 	var app = angular.module('todoApp', [])
   	 app.controller('todoController', function($scope){
-        $scope.tasks = localStorage.getItem('todo')!==null);
+      //  const tasks = JSON.parse(localStorage.getItem('todo')) : []
+			$scope.saved = localStorage.getItem('todo');
+			$scope.tasks = (localStorage.getItem('todo')!==null) ? JSON.parse(localStorage.getItem('todo')) : [];
+			localStorage.setItem('todo', JSON.stringify($scope.tasks));
+
 				console.log($scope.tasks);
         $scope.complete = [];
         $scope.taskComplete = false;
         $scope.visible = false;
+
+
+
 
         $scope.add = function() {
             $scope.tasks.push({name:$scope.taskName, date:$scope.dueDate});
@@ -20,6 +27,7 @@
 
         $scope.completed = function(){
           $scope.complete.push($scope.tasks[this.$index]);
+
           $scope.tasks.splice(this.$index, 1);
           $scope.taskComplete = true;
         }
