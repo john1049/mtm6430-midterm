76696ce41f4fcc298f746e80c3bbb17fc7e5d347
LAPTOP\Ryan <john1049@algonquinlive.com>
2017-03-09 18:41:24 -0500

    cleaned up code

--- midterm-wa/todo.js
+++ midterm-wa/todo.js
@@ -1,44 +1,38 @@
-(function(){
-	var app = angular.module('todoApp', [])
-  	 app.controller('todoController', function($scope){
-			$scope.tasks = (localStorage.getItem('todo')!= null) ? JSON.parse(localStorage.getItem('todo')) : [];
-			$scope.complete = (localStorage.getItem('complete')!= null) ? JSON.parse(localStorage.getItem('complete')) : [];
-			console.log($scope.complete);
-        $scope.taskComplete = false;
+(function() {
+    var app = angular.module('todoApp', [])
+    app.controller('todoController', function($scope) {
+        $scope.tasks = (localStorage.getItem('todo') != null) ? JSON.parse(localStorage.getItem('todo')) : [];
+        $scope.complete = (localStorage.getItem('complete') != null) ? JSON.parse(localStorage.getItem('complete')) : [];
         $scope.visible = false;
 
-
-				$scope.update = function(){
-					localStorage.setItem('todo', JSON.stringify($scope.tasks));
-				}
+        $scope.update = function() {
+            localStorage.setItem('todo', JSON.stringify($scope.tasks));
+        }
 
         $scope.add = function() {
-            $scope.tasks.push({name:$scope.taskName, date:$scope.dueDate});
-						$scope.update();
+            $scope.tasks.push({
+                name: $scope.taskName,
+                date: $scope.dueDate
+            });
+            $scope.update();
             $scope.taskName = '';
             $scope.dueDate = '';
         }
 
-        $scope.delete = function(){
-          $scope.tasks.splice(this.$index, 1);
-					$scope.update();
+        $scope.delete = function() {
+            $scope.tasks.splice(this.$index, 1);
+            $scope.update();
         }
 
-        $scope.completed = function(){
-          $scope.complete.push($scope.tasks[this.$index]);
-					localStorage.setItem('complete', JSON.stringify($scope.complete));
-          $scope.tasks.splice(this.$index, 1);
-					$scope.update();
-
-					console.log('complete' + $scope.complete);
-					console.log('todo' + $scope.tasks);
-
-          $scope.taskComplete = true;
+        $scope.completed = function() {
+            $scope.complete.push($scope.tasks[this.$index]);
+            localStorage.setItem('complete', JSON.stringify($scope.complete));
+            $scope.tasks.splice(this.$index, 1);
+            $scope.update();
         }
 
-				$scope.deleteComplete = function(){
-          $scope.complete = [];
+        $scope.deleteComplete = function() {
+            $scope.complete = [];
         }
-
     })
-	})();
+})();
