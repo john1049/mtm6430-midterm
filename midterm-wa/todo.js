5ebfd4ef3284b83bec4d0d0bfa3d2e46969566af
LAPTOP\Ryan <john1049@algonquinlive.com>
2017-03-09 15:09:45 -0500

    Test localStorage functionality for add function

--- midterm-wa/todo.js
+++ midterm-wa/todo.js
@@ -1,13 +1,15 @@
 (function(){
 	var app = angular.module('todoApp', [])
   	 app.controller('todoController', function($scope){
-        $scope.tasks = [];
+        $scope.tasks = localStorage.getItem('todo')!==null);
+				console.log($scope.tasks);
         $scope.complete = [];
         $scope.taskComplete = false;
         $scope.visible = false;
 
         $scope.add = function() {
             $scope.tasks.push({name:$scope.taskName, date:$scope.dueDate});
+						localStorage.setItem('todo', JSON.stringify($scope.tasks));
             $scope.taskName = '';
             $scope.dueDate = '';
         }
